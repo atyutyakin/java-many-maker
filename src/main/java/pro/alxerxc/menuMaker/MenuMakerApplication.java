6fa0b6dfcddc3a7bd97efea9a917cc3315146ed5
Alexey Tyutyakin <alxerxc@gmail.com>
2022-04-01 21:49:36 +0700

    Initial commit

---
+++ src/main/java/pro/alxerxc/menuMaker/MenuMakerApplication.java
@@ -0,0 +1,13 @@
+package pro.alxerxc.menuMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/alxerxc/menuMaker/MenuMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.alxerxc.menuMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
